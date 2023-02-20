71c315d0a54c6448935393214b44a5c9db8b363d
Dadir Perez <dadir716@gmail.com>
2023-02-19 23:02:15 -0600

    inicializacion de app. prueba1

--- app/src/main/java/com/example/mynotes/MainActivity.kt
+++ app/src/main/java/com/example/mynotes/MainActivity.kt
@@ -2,13 +2,32 @@ package com.example.mynotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mynotes.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
     private lateinit var binding : ActivityMainBinding
+        private lateinit var noteAdapter: NoteAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val data = mutableListOf(
+            Note(1,"Estudiar"),
+            Note(2,"Ir al mercado"))
+
+        noteAdapter = NoteAdapter(data, this)
+        binding.recycleView.apply {
+            layoutManager = LinearLayoutManager (this@MainActivity)
+            adapter = noteAdapter
+        }
+
+    }
+
+    override fun onLongClick(note: Note) {
+
     }
 }
--- app/src/main/java/com/example/mynotes/NoteAdapter.kt
+++ app/src/main/java/com/example/mynotes/NoteAdapter.kt
@@ -1,10 +1,30 @@
 package com.example.mynotes
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mynotes.databinding.ItemNoteBinding
 
-class NoteAdapter {
+class NoteAdapter (var noteList:MutableList<Note>,private val listener : OnClickListener) :
+    RecyclerView.Adapter<NoteAdapter.ViewHolder> (){
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_note,parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val note = noteList.get(position)
+
+        holder.binding.tvDescription.text = note.description
+    }
+
+    override fun getItemCount(): Int = noteList.size
+
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
         val binding = ItemNoteBinding.bind(view)
